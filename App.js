6badd1542f4fd501230548087349a5a6ba3ea435
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-12-09 14:43:38 +0530

    create task items

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Item from './components/Item';
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <View style={styles.container}>
-      
+      <View style={styles.tasks}>
+        <Text style={styles.title}>My Tasks</Text>
+        <View style={styles.items}>
+          {/* items here */}
+          <Item text={'Task 1'} />
+          <Item text={'Task 2'} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +20,21 @@ export default function App(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#cee0df',
+  },
+
+  tasks: {
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+
+  items: {
+    marginTop: 20,
   },
 });
